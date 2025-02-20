99b1cb6160fc3616ba49e80a36aac5cff66c59a1
aazhnaspam <aashnaspam12@gmail.com>
2025-02-20 16:05:27 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -2,15 +2,15 @@ let gift = document.querySelector('.gift');
 gift.addEventListener("click",function() {
     window.location.href = "present.html"; })
 
-    document.addEventListener("DOMContentLoaded", function () {
-    const audio = document.getElementById("background-audio");
+//     document.addEventListener("DOMContentLoaded", function () {
+//     const audio = document.getElementById("background-audio");
 
-    // Try to unmute and play after a short delay
-    setTimeout(() => {
-        audio.muted = false;
-        audio.play().catch(err => console.log("Autoplay blocked:", err));
-    }, 1000);
-});
+//     // Try to unmute and play after a short delay
+//     setTimeout(() => {
+//         audio.muted = false;
+//         audio.play().catch(err => console.log("Autoplay blocked:", err));
+//     }, 1000);
+// });
 
 
 
