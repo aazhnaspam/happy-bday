639bac8c7948ce429530f5ee2e4c901d029bded1
aazhnaspam <aashnaspam12@gmail.com>
2025-02-20 14:27:01 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -2,10 +2,20 @@ let gift = document.querySelector('.gift');
 gift.addEventListener("click",function() {
     window.location.href = "present.html"; })
 
-    document.addEventListener("DOMContentLoaded", function () {
-        const audio = document.getElementById("background-audio");
-        // Unmute after a short delay to ensure autoplay works
-        setTimeout(() => {
-          audio.muted = false;
-        }, 1000);
-      });
+    // document.addEventListener("DOMContentLoaded", function () {
+    //     const audio = document.getElementById("background-audio");
+    //     // Unmute after a short delay to ensure autoplay works
+    //     setTimeout(() => {
+    //       audio.muted = false;
+    //     }, 1000);
+    //   });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const audio = document.getElementById("background-audio");
+
+    setTimeout(() => {
+        audio.muted = false;
+        audio.play().catch(err => console.log("Playback error:", err));
+    }, 1000);
+});
+
